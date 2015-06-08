f043eb6c7379c3c0154941b3d6b37f0ad08be8dc
gunnar <gunnar.forsgren@mobimation.com>
2015-06-09 01:39:40 +0200

    Adding http sync tests

---
+++ app/src/main/java/tv/laidback/cheaprace2015/AsyncHttpPostTask.java
@@ -0,0 +1,87 @@
+package tv.laidback.cheaprace2015;
+
+import android.content.Context;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Upload a file to a http server in an asynchronous way.
+ * An example would be
+ * new AsyncHttpPostTask("http://lilldata.se/cheaprace/hub").execute(new File("movie.mp4"));
+ */
+public class AsyncHttpPostTask extends AsyncTask<File, Void, String> {
+
+    private static final String TAG = AsyncHttpPostTask.class.getSimpleName();
+    private String server;
+    private Context context;
+
+    public AsyncHttpPostTask(final Context c, final String server) {
+        this.server = server;
+        this.context = c;
+    }
+
+    @Override
+    protected String doInBackground(File... params) {
+        Log.d(TAG, "doInBackground");
+        HttpClient http = AndroidHttpClient.newInstance("MyApp");
+        HttpPost method = new HttpPost(this.server);
+        method.setEntity(new FileEntity(params[0], "text/plain"));
+        try {
+            HttpResponse response = http.execute(method);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+            final StringBuilder out = new StringBuilder();
+            String line;
+            try {
+                while ((line = rd.readLine()) != null) {
+                    out.append(line);
+                }
+            } catch (Exception e) {}
+            // wr.close();
+            try {
+                rd.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // final String serverResponse = slurp(is);
+            Log.d(TAG, "serverResponse: " + out.toString());
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private File getVideo() {
+        File file = new File(context.getFilesDir() + File.separator + "linaanna.mp4");
+        try {
+            InputStream inputStream = context.getResources().openRawResource(R.raw.linaanna);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            byte buf[]=new byte[1024];
+            int len;
+            while((len=inputStream.read(buf))>0) {
+                fileOutputStream.write(buf,0,len);
+            }
+
+            fileOutputStream.close();
+            inputStream.close();
+
+        } catch (IOException e1) {}
+        return file;
+    }
+}
---
+++ app/src/main/java/tv/laidback/cheaprace2015/SyncTestFragment.java
@@ -0,0 +1,43 @@
+package tv.laidback.cheaprace2015;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SyncTestFragment extends Fragment {
+
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    static String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static SyncTestFragment newInstance(int sectionNumber) {
+        SyncTestFragment fragment = new SyncTestFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SyncTestFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_teams, container, false);
+    }
+}
